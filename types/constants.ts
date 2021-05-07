d7789f87aa0e42b89f286757b1e23a8f3f68af04
roohei <roope.heinonen@cgi.com>
2021-05-07 13:18:38 +0300

    added url link consts for footer, removed test stuff

--- types/constants.ts
+++ types/constants.ts
@@ -1,9 +1,7 @@
 // place for general constrants e.g const and enums
+// all 'static text' should be here
 
-// delete later
-export enum exampleType {
-  Representative = "representative",
-  NotRepresentativ = "notrepresentative",
-}
+export const TERMS_URL = "https://myhelsinki.fi/fi/myhelsinki-places-palvelun-käyttöehdot/";
+export const ACCESSIBILITY_URL = "https://myhelsinki.fi/fi/saavutettavuusseloste/";
 
 export default null;
