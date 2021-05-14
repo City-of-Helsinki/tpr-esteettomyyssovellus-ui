beeff7da8128d3ffcb00fabeba86359151612238
Kaunisto <leevi.kaunisto@cgi.com>
2021-05-14 10:25:34 +0300

    Add styling and functionality to header. Add responsiveness to mobile.

--- state/store.ts
+++ state/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import exampleSlice from "./reducers/exampleSlice";
+import { userSlice } from "./reducers/userSclice";
 
 // delete example reducer & exampleSlice just an example for toolkit
 
 export const store = configureStore({
   reducer: {
     exampleReducer: exampleSlice,
+    //general: userSlice
   },
 });
 
