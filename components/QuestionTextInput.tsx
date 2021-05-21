5144c82ed9f71e84d7843d505d8c10a2e16af1ff
Kaunisto <leevi.kaunisto@cgi.com>
2021-05-21 13:41:03 +0300

    Fixes in keyboard accessibility.

--- components/QuestionTextInput.tsx
+++ components/QuestionTextInput.tsx
@@ -3,7 +3,7 @@ import { TextInput } from "hds-react";
 import { QuestionTextInputProps } from "../types/general";
 
 const QuestionTextInput = ({ id, label, placeholder, helperText, required = false }: QuestionTextInputProps): JSX.Element => {
-  return <TextInput id={id} label={label} placeholder={placeholder} helperText={helperText} required={required} />;
+  return <TextInput id={id} label={label} placeholder={placeholder} helperText={helperText} required={required}/>;
 };
 
 export default QuestionTextInput;
