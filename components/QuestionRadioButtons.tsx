549eded039130ae9ceac0b138ba69b30d3b94665
roohei <roope.heinonen@cgi.com>
2021-06-08 17:08:51 +0300

    map components and minor changes to other components

--- components/QuestionRadioButtons.tsx
+++ components/QuestionRadioButtons.tsx
@@ -1,9 +1,10 @@
 import { SelectionGroup, RadioButton } from "hds-react";
 import React, { useState } from "react";
 import { useI18n } from "next-localization";
+import { QuestionRadioButtonsProps } from "../types/general";
 
-const QuestionRadioButtons = (): JSX.Element => {
-  // todo: maybe take in props
+const QuestionRadioButtons = ({ mainLabel, firstButtonLabel = "", secondButtonLabel = "" }: QuestionRadioButtonsProps): JSX.Element => {
+  // todo: maybe take in prost
   const i18n = useI18n();
   const [selectedRadioItem, setSelectedRadioItem] = useState("0");
 
@@ -12,9 +13,9 @@ const QuestionRadioButtons = (): JSX.Element => {
   };
   return (
     // TODO: id, name, value, label (?) for radiobuttons needs to be set programatically with data
-    <SelectionGroup direction="horizontal">
-      <RadioButton id="v-radio1" name="v-radio" value="1" label="PH: kyllä" checked={selectedRadioItem === "1"} onChange={handleRadioClick} />
-      <RadioButton id="v-radio2" name="v-radio" value="2" label="PH: ei" checked={selectedRadioItem === "2"} onChange={handleRadioClick} />
+    <SelectionGroup direction="horizontal" label={mainLabel}>
+      <RadioButton id="v-radio1" name="v-radio" value="1" label={firstButtonLabel} checked={selectedRadioItem === "1"} onChange={handleRadioClick} />
+      <RadioButton id="v-radio2" name="v-radio" value="2" label={secondButtonLabel} checked={selectedRadioItem === "2"} onChange={handleRadioClick} />
     </SelectionGroup>
   );
 };
