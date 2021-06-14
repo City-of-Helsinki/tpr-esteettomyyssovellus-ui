f89e0e03e4dcc5dee97c77b135dd54a06b0e2478
Kaunisto <leevi.kaunisto@cgi.com>
2021-06-14 15:50:39 +0300

    Add basic logic to /servicepoint to fetch data from the backend application's API.

--- pages/servicepoint.tsx
+++ pages/servicepoint.tsx
@@ -12,9 +12,10 @@ import ServicepointLandingSummaryCtrlButtons from "../components/ServicepointLan
 import QuestionInfo from "../components/QuestionInfo";
 import ServicepointMainInfoContent from "../components/ServicepointMainInfoContent";
 
-const Servicepoint = (): ReactElement => {
+const Servicepoint = ({servicepointData}: any): ReactElement => {
   const i18n = useI18n();
 
+  // TODO: Modify the format of the values displayed on the website.
   return (
     <Layout>
       <Head>
@@ -34,12 +35,13 @@ const Servicepoint = (): ReactElement => {
             </QuestionInfo>
           </div>
           <div className={styles.headingcontainer}>
-            <h1>PH: Aitohoiva - Esteettömyystietojen yhteenveto</h1>
-            <h2>PH: Pääsisäänkäynti: Katukatu 12, 00100 Helsinki</h2>
+            <h1>{servicepointData.servicepoint_name}</h1>
+            <h2>Pääsisäänkäynti: {servicepointData.address_street_name} {servicepointData.address_no}, 00100 {servicepointData.address_city}</h2>
             <span className={styles.statuslabel}>
               {/* TODO: change statuslabel with data respectively */}
               <StatusLabel type="success"> PH: Valmis </StatusLabel>
-              <p>PH: päivitetty 31.01.1780</p>
+              {/* TODO: modify to format:  31.01.1780 */}
+              <p>päivitetty {servicepointData.modified}</p>
             </span>
           </div>
           <div>
@@ -63,6 +65,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locales }) =
   // reduxStore.dispatch({ type: CLEAR_STATE });
   const initialReduxState = reduxStore.getState();
 
+
+  const res = await fetch('http://localhost:8000/api/ArServicepoints/69/?format=json')
+  const servicepointData = await res.json();
   // const user = await checkUser(req);
   // if (!user) {
   //   // Invalid user but login is not required
@@ -75,6 +80,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locales }) =
     props: {
       initialReduxState,
       lngDict,
+      servicepointData
     },
   };
 };
