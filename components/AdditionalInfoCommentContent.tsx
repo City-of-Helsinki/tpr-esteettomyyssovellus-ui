e60851e320224607e9eb457672d9b91209ca8e40
roohei <roope.heinonen@cgi.com>
2021-06-18 11:34:56 +0300

    additional info page and component related stuff, state for all but location

--- components/AdditionalInfoCommentContent.tsx
+++ components/AdditionalInfoCommentContent.tsx
@@ -1,28 +1,92 @@
 import React from "react";
-import { IconPlus, IconMinus } from "hds-react"
+import { IconPlus, IconMinus, IconCross } from "hds-react";
 import { useI18n } from "next-localization";
 import styles from "./AdditionalInfoCommentContent.module.scss";
 import { TextArea } from "hds-react";
 import QuestionInfo from "./QuestionInfo";
-import { AdditionalContentProps } from "../types/general"
+import { AdditionalContentProps } from "../types/general";
+import { addComment, removeComment } from "../state/reducers/additionalInfoSlice";
+import { useAppDispatch } from "../state/hooks";
+import QuestionButton from "./QuestionButton";
 
-const AdditionalInfoCommentContent = ({ questionNumber }: AdditionalContentProps ): JSX.Element => {
+const AdditionalInfoCommentContent = ({ questionNumber, onDelete }: AdditionalContentProps): JSX.Element => {
   const i18n = useI18n();
+  const dispatch = useAppDispatch();
+
+  // on delete button clicked chain delete image from store and delete component cb
+  const handleOnDelete = () => {
+    handleRemoveComment();
+    onDelete ? onDelete() : null;
+  };
+
+  const handleRemoveComment = () => {
+    dispatch(removeComment({ questionNumber }));
+  };
+
+  // only update state after 1 sec from prev KeyDown, set Alt text with correct lang
+  let timer: NodeJS.Timeout;
+  const handleAddComment = (e: React.KeyboardEvent<HTMLTextAreaElement>, language: string) => {
+    const value: string = e.currentTarget.value;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      dispatch(addComment({ questionNumber, language, value }));
+    }, 500);
+  };
 
   return (
     <div className={styles.maincontainer}>
       <div className={styles.contentcontainer}>
         <div className={styles.descriptioncontainer}>
-            <p>PH: voit tarkentaa esteettömyystietoja vapaamuotoisella lisätekstillä</p>
+          <h4>PH: voit tarkentaa esteettömyystietoja vapaamuotoisella lisätekstillä</h4>
         </div>
-        <div className={styles.commentscontainer}>
-            <TextArea id="comment-1" label="ph: Kuvaile, mitä kuva esittää (alt-teksti) " helperText="Lisäteksti" required draggable/>
-            <QuestionInfo openText="ph: Lisää teksti ruotsiksi" closeText="sulje kuvailu" openIcon={<IconPlus/>} closeIcon={<IconMinus/>} textOnBottom>
-                <TextArea id="comment-2"  label="ph: lisäteksti ruotsiksi *" helperText="Kerro lisää esteettömyydestä ruotsiksi"/>
-            </QuestionInfo>
-            <QuestionInfo openText="ph: Lisää teksti englanniksi" closeText="sulje kuvailu" openIcon={<IconPlus/>} closeIcon={<IconMinus/>} textOnBottom>
-                <TextArea id="comment-3"  label="ph: teksti englanniksi *" helperText="Kerro lisää esteettömyydestä englanniksi"/>
-            </QuestionInfo>
+        <div className={styles.flexcontainer}>
+          <div className={styles.commentscontainer}>
+            <TextArea
+              id="comment-1"
+              label="ph: Kuvaile, mitä kuva esittää (alt-teksti) "
+              helperText="Lisäteksti"
+              required
+              tooltipButtonLabel={i18n.t("additionalInfo.generalTooptipButtonLabel")}
+              tooltipLabel={i18n.t("additionalInfo.generalTooptipLabel")}
+              tooltipText={i18n.t("additionalInfo.altToolTipContent")}
+              onKeyUp={(e: React.KeyboardEvent<HTMLTextAreaElement>) => handleAddComment(e, "comment_fi")}
+            />
+            <div className={styles.optionalaltscontainer}>
+              <QuestionInfo
+                openText="ph: Lisää teksti ruotsiksi"
+                closeText="sulje kuvailu"
+                openIcon={<IconPlus />}
+                closeIcon={<IconMinus />}
+                textOnBottom
+              >
+                <TextArea
+                  id="comment-2"
+                  label="ph: lisäteksti ruotsiksi *"
+                  helperText="Kerro lisää esteettömyydestä ruotsiksi"
+                  onKeyUp={(e: React.KeyboardEvent<HTMLTextAreaElement>) => handleAddComment(e, "comment_sv")}
+                />
+              </QuestionInfo>
+              <QuestionInfo
+                openText="ph: Lisää teksti englanniksi"
+                closeText="sulje kuvailu"
+                openIcon={<IconPlus />}
+                closeIcon={<IconMinus />}
+                textOnBottom
+              >
+                <TextArea
+                  id="comment-3"
+                  label="ph: teksti englanniksi *"
+                  helperText="Kerro lisää esteettömyydestä englanniksi"
+                  onKeyUp={(e: React.KeyboardEvent<HTMLTextAreaElement>) => handleAddComment(e, "comment_en")}
+                />
+              </QuestionInfo>
+            </div>
+          </div>
+          <div className={styles.buttonscontainer}>
+            <QuestionButton variant="secondary" iconRight={<IconCross />} onClickHandler={() => handleOnDelete()}>
+              PH: Peruteksti
+            </QuestionButton>
+          </div>
         </div>
       </div>
     </div>
--- state/reducers/additionalInfoSlice.ts
+++ state/reducers/additionalInfoSlice.ts
@@ -12,25 +12,22 @@ const initialState = {
         coordinates: null,
       },
     ],
-    comments: [
-      {
-        fi: "",
-        en: "",
-        sv: "",
-      },
-    ],
+    comments: {
+      comment_fi: "",
+      comment_en: "",
+      comment_sv: "",
+    },
     pictures: [
       {
         id: 0,
-        base64: "",
+        base: "",
         url: "",
-        altTextLocales: {
-          fi: "",
-          en: "",
-          sv: "",
-        },
+        alt_fi: "",
+        alt_en: "",
+        alt_sv: "",
       },
     ],
+    components: [],
   },
 };
 
@@ -48,37 +45,127 @@ export const additionalInfoSlice = createSlice({
     ) => {
       // TODO: set state and change payload (?)
     },
-    addPicture: (
-      state,
-      action: PayloadAction<PictureProps>
-    ) => {
-      const qNumber = action.payload.qNumber 
-        return {
-          ...state,
-          [qNumber]: {
-            pictures: [...state[qNumber]?.pictures ?? [], action.payload] 
-          }
-        }
+    addPicture: (state, action: PayloadAction<PictureProps>) => {
+      const qNumber = action.payload.qNumber;
+      return {
+        [qNumber]: {
+          ...state[qNumber],
+          pictures: [...(state[qNumber]?.pictures ?? []), action.payload],
+        },
+      };
     },
-    removePicture: (
-      state,
-      action: PayloadAction<{questionNumber: string, currentId: string}>
-    ) => {
-      const qNumber = action.payload.questionNumber
-      const id = action.payload.currentId
+    removeComment: (state, action: PayloadAction<{ questionNumber: string }>) => {
+      const qNumber = action.payload.questionNumber;
+      return {
+        [qNumber]: {
+          ...state[qNumber],
+          comments: {},
+        },
+      };
+    },
+    removePicture: (state, action: PayloadAction<{ questionNumber: string; currentId: number }>) => {
+      const qNumber = action.payload.questionNumber;
+      const id = action.payload.currentId;
+      return {
+        [qNumber]: {
+          ...state[qNumber],
+          pictures: [...(state[qNumber].pictures?.filter((picture) => picture.id !== id) ?? [])],
+        },
+      };
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    addComment: (state, action: PayloadAction<{ questionNumber: string; language: string; value: string }>) => {
+      const qNumber = action.payload.questionNumber;
+      const lang = action.payload.language;
+      const value = action.payload.value;
+
       return {
         ...state,
         [qNumber]: {
-          pictures: [...state[qNumber].pictures?.filter(item => item.id !== id) ?? []]
-        }
-      }
+          ...state[qNumber],
+          comments: { ...state[qNumber].comments, [lang]: value },
+        },
+      };
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    addComment: (state, action: PayloadAction<number>) => {},
+    addComponent: (state, action: PayloadAction<{ questionNumber: string; type: string; id: number }>) => {
+      const qNumber = action.payload.questionNumber;
+      const type = action.payload.type;
+      const id = action.payload.id;
+      return {
+        [qNumber]: {
+          ...state[qNumber],
+          components: [...(state[qNumber]?.components ?? []), { id: id, type: type }],
+        },
+      };
+    },
+    removeComponent: (state, action: PayloadAction<{ questionNumber: string; delId: number }>) => {
+      const qNumber = action.payload.questionNumber;
+      const id = action.payload.delId;
+      return {
+        ...state,
+        [qNumber]: {
+          ...state[qNumber],
+          components: [...(state[qNumber]?.components?.filter((elem) => elem.id !== id) ?? [])],
+        },
+      };
+    },
+    setAlt: (state, action: PayloadAction<{ questionNumber: string; language: string; value: string; compId: number }>) => {
+      const qNumber = action.payload.questionNumber;
+      const lang = action.payload.language;
+      const value = action.payload.value;
+      const id = action.payload.compId;
+
+      let targetPic = state[qNumber].pictures?.find((pic) => pic.id === id);
+
+      if (!targetPic) {
+        return { ...state };
+      }
+
+      targetPic = { ...targetPic, [lang]: value };
+
+      return {
+        ...state,
+        [qNumber]: {
+          ...state[qNumber],
+          pictures: [...(state[qNumber].pictures?.filter((pic) => pic.id !== id) || []), targetPic],
+        },
+      };
+    },
+    setPictureSource: (state, action: PayloadAction<{ questionNumber: string; compId: number; source: string }>) => {
+      const qNumber = action.payload.questionNumber;
+      const source = action.payload.source;
+      const id = action.payload.compId;
+
+      let targetPic = state[qNumber].pictures?.find((pic) => pic.id === id);
+
+      if (!targetPic) {
+        return { ...state };
+      }
+
+      targetPic = { ...targetPic, source: source };
+
+      return {
+        ...state,
+        [qNumber]: {
+          ...state[qNumber],
+          pictures: [...(state[qNumber].pictures?.filter((pic) => pic.id !== id) || []), targetPic],
+        },
+      };
+    },
   },
 });
 
-export const { addLocation, addPicture, removePicture, addComment } = additionalInfoSlice.actions;
+export const {
+  addLocation,
+  addPicture,
+  removePicture,
+  addComment,
+  addComponent,
+  removeComponent,
+  setAlt,
+  setPictureSource,
+  removeComment,
+} = additionalInfoSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState): number => state.exampleReducer.value;
