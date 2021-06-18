2fd450ca030d45c10c7ed9cbdc4e970c1c3028ae
roohei <roope.heinonen@cgi.com>
2021-06-18 11:33:29 +0300

    removed textinput for it was same as in HDS, minor modifies Questionbutton

--- components/QuestionTextInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { TextInput } from "hds-react";
-import { QuestionTextInputProps } from "../types/general";
-
-const QuestionTextInput = ({ id="0", label, placeholder, helperText, required = false, disabled = false }: QuestionTextInputProps): JSX.Element => {
-  return <TextInput id={id} label={label} placeholder={placeholder} helperText={helperText} required={required} disabled={disabled}/>;
-};
-
-export default QuestionTextInput;
--- components/SearchBoxWithButtonsMobile.tsx
+++ components/SearchBoxWithButtonsMobile.tsx
@@ -1,37 +1,29 @@
-import React, { ChangeEvent} from "react";
+import React, { ChangeEvent } from "react";
 import { IconSearch, IconAngleDown, TextInput } from "hds-react";
 import styles from "./SearchBoxWithButtonsMobile.module.scss";
-import QuestionTextInput from "./QuestionTextInput";
 import Button from "./QuestionButton";
 
-
 const SearchBoxWithButtonsMobile = (): JSX.Element => {
-
-
-    return (
-        <>
-            <div className={styles.container}>
-                <p className={styles.searchText}> Hae toimipistettä</p>
-                <div className={styles.input}>
-                    <QuestionTextInput  
-                        id="search" placeholder="Anna toimipisteen nimi tai joku muu hakusana"/>
-                </div>
-                <div className={styles.buttons}>
-                    <div className={styles.button}>
-                        <Button variant="primary" iconLeft={<IconSearch />}>
-                            Hae toimipistettä
-                        </Button>
-                    </div>
-                    <div id="empty" className={styles.button}>
-                        <Button variant="secondary">
-                            Tyhjennä haku
-                        </Button> 
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.container}>
+        <p className={styles.searchText}> Hae toimipistettä</p>
+        <div className={styles.input}>
+          <TextInput id="search" placeholder="Anna toimipisteen nimi tai joku muu hakusana" />
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.button}>
+            <Button variant="primary" iconLeft={<IconSearch />}>
+              Hae toimipistettä
+            </Button>
+          </div>
+          <div id="empty" className={styles.button}>
+            <Button variant="secondary">Tyhjennä haku</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-
 
 export default SearchBoxWithButtonsMobile;
