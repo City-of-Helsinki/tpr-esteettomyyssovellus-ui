f097107c7d2c2bf618d582fab63bd11fcb81de72
roohei <roope.heinonen@cgi.com>
2021-08-20 13:19:18 +0300

    some changes broke the form, removed them

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ import store, { persistor } from "../state/store";
 import "../styles/global.scss";
 
 import { PersistGate } from "redux-persist/integration/react";
-import { ThemeProvider } from "@material-ui/core";
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   // This function is called when doing both server-side and client-side rendering
@@ -27,11 +26,7 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
     <I18nProvider lngDict={lngDict ? lngDict[locale] : {}} locale={locale}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          {() => (
-            <ThemeProvider theme={{}}>
-              <Component {...rest} />
-            </ThemeProvider>
-          )}
+          <Component {...rest} />
         </PersistGate>
       </Provider>
     </I18nProvider>
