d3ae70267e522c9c0b2e54450d1fb878246a8e1c
roohei <roope.heinonen@cgi.com>
2021-08-20 12:20:23 +0300

    some fixes to redux-persist, still not 100%

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import store, { persistor } from "../state/store";
 import "../styles/global.scss";
 
 import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "@material-ui/core";
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   // This function is called when doing both server-side and client-side rendering
@@ -26,7 +27,11 @@ const App = ({ Component, pageProps }: AppProps): ReactElement => {
     <I18nProvider lngDict={lngDict ? lngDict[locale] : {}} locale={locale}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Component {...rest} />
+          {() => (
+            <ThemeProvider theme={{}}>
+              <Component {...rest} />
+            </ThemeProvider>
+          )}
         </PersistGate>
       </Provider>
     </I18nProvider>
