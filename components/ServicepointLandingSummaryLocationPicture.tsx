d8c6b735533a5055a09f268628ac6ffee52f3cdd
Anthony Brown <ant.brown@cgi.com>
2022-01-31 11:01:44 +0200

    Removed coordinates from below service point summary map

--- components/ServicepointLandingSummaryLocationPicture.tsx
+++ components/ServicepointLandingSummaryLocationPicture.tsx
@@ -22,9 +22,9 @@ const ServicepointLandingSummaryLocationPicture = ({ entranceKey, entranceData }
         <h4>{entranceKey === "main" ? i18n.t("servicepoint.mainEntranceLocationLabel") : i18n.t("servicepoint.entranceLocationLabel")}</h4>
         <div className={styles.map}>
           <Map initCenter={coordinates} initLocation={coordinates} initZoom={17} draggableMarker={false} questionId={-1} makeStatic />
-          {coordinates && coordinates.length === 2 ? (
+          {/*coordinates && coordinates.length === 2 ? (
             <p>{`${i18n.t("servicepoint.mainEntranceLocationCoordinatesLabel")}: E ${coordinates[0].toFixed(2)}, N ${coordinates[1].toFixed(2)}`}</p>
-          ) : null}
+          ) : null*/}
         </div>
       </div>
 
