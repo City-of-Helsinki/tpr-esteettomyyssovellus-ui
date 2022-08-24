5d6e9b3916b641f9ff5d9b98fa1d5fd32b1e61d0
Anthony Brown <ant.brown@cgi.com>
2022-08-24 15:45:13 +0300

    Changed footer to match new design

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import { useI18n } from "next-localization";
-import { Footer as HdsFooter, IconAngleRight, IconArrowUp } from "hds-react";
+import { Footer as HdsFooter, IconArrowUp, IconLinkExternal, Logo } from "hds-react";
 import { useAppDispatch } from "../../state/hooks";
 import { setHelpOpen } from "../../state/reducers/generalSlice";
 import { ACCESSIBILITY_URL, TERMS_URL } from "../../types/constants";
@@ -17,23 +17,23 @@ const Footer = (): ReactElement => {
   return (
     <HdsFooter korosType="storm" className={styles.footer} title={i18n.t("common.footer.title")}>
       <HdsFooter.Navigation variant="minimal">
-        <div className={styles.tprlogoContainer} aria-hidden>
-          <div className={styles.tprlogo} />
-          <h2 title="tpr logo" role="img">
-            {i18n.t("common.footer.logotext")}
-          </h2>
-        </div>
         <HdsFooter.Item as="a" href="#help" onClick={openHelp} label={i18n.t("common.footer.instructions")} icon={<IconArrowUp aria-hidden />} />
         <HdsFooter.Item as="a" href="#content" label={i18n.t("common.footer.backToTop")} icon={<IconArrowUp aria-hidden />} />
       </HdsFooter.Navigation>
       <HdsFooter.Base copyrightHolder={i18n.t("common.footer.copyright")} copyrightText={i18n.t("common.footer.rightsReserved")}>
+        <HdsFooter.Item as="div" className={styles.helsinkiLogoItem}>
+          <div className={styles.helsinkiLogoContainer} aria-hidden>
+            <Logo size="medium" />
+            <span className={styles.helsinkiLogoText}>{i18n.t("common.footer.logotext")}</span>
+          </div>
+        </HdsFooter.Item>
         <HdsFooter.Item
           as="a"
           href={ACCESSIBILITY_URL}
           label={i18n.t("common.footer.accessibility")}
-          icon={<IconAngleRight aria-hidden role="link" />}
+          icon={<IconLinkExternal aria-hidden role="link" />}
         />
-        <HdsFooter.Item as="a" href={TERMS_URL} label={i18n.t("common.footer.terms")} icon={<IconAngleRight aria-hidden />} role="link" />
+        <HdsFooter.Item as="a" href={TERMS_URL} label={i18n.t("common.footer.terms")} icon={<IconLinkExternal aria-hidden />} role="link" />
       </HdsFooter.Base>
     </HdsFooter>
   );
