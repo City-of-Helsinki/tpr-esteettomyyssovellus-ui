7093320fcbb892dd9e715709b6dcf4e792387582
Anthony Brown <ant.brown@cgi.com>
2022-10-03 15:44:41 +0300

    Set header url to same page so that link has some href

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -68,6 +68,7 @@ const Header = ({ isSummary, children }: HeaderProps): ReactElement => {
         title={i18n.t("common.header.title")}
         titleAriaLabel={i18n.t("common.header.title")}
         // titleUrl={`${router.basePath}/${router.locale}`}
+        titleUrl={`${router.asPath}`}
         menuToggleAriaLabel="menu"
         skipTo="#content"
         skipToContentLabel={i18n.t("common.header.skipToContent")}
