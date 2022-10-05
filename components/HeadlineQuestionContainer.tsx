408f330f6b2803e64812cc34efbd776628a92339
Anthony Brown <ant.brown@cgi.com>
2022-10-05 19:02:35 +0300

    Improved question block validity icon accessibility

--- components/HeadlineQuestionContainer.tsx
+++ components/HeadlineQuestionContainer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useAccordion, Button, IconMinus, IconPlus, IconCheckCircleFill, Card, IconAlertCircle } from "hds-react";
+import { useI18n } from "next-localization";
 import { HeadlineQuestionContainerProps } from "../types/general";
 import styles from "./HeadlineQuestionContainer.module.scss";
 import { useAppSelector } from "../state/hooks";
 
 // usage: used for mainlevel (blue) accordions in form
 const HeadlineQuestionContainer = ({ text, number, initOpen = false, children, isValid, id = "" }: HeadlineQuestionContainerProps): JSX.Element => {
+  const i18n = useI18n();
+
   // Handle accordion state with useAccordion hook
   const { isOpen, buttonProps, contentProps, toggleAccordion } = useAccordion({
     initiallyOpen: initOpen,
@@ -15,9 +18,9 @@ const HeadlineQuestionContainer = ({ text, number, initOpen = false, children, i
   const curInvalidBlocks = useAppSelector((state) => state.formReducer.invalidBlocks);
 
   // Change icon based on accordion open state
-  const icon = isOpen ? <IconMinus aria-hidden /> : <IconPlus aria-hidden />;
-  let iconLeft = curFinishedBlocks.includes(Number(number)) ? <IconCheckCircleFill aria-hidden /> : null;
-  iconLeft = !isValid ? <IconAlertCircle className={styles.invalidIcon} aria-hidden /> : iconLeft;
+  const iconRight = isOpen ? <IconMinus aria-hidden /> : <IconPlus aria-hidden />;
+  let iconLeft = curFinishedBlocks.includes(Number(number)) ? <IconCheckCircleFill aria-label={i18n.t("common.message.valid")} /> : null;
+  iconLeft = !isValid ? <IconAlertCircle className={styles.invalidIcon} aria-label={i18n.t("common.message.invalid")} /> : iconLeft;
   let buttonVariant: "primary" | "secondary" | "danger" = isOpen ? "primary" : "secondary";
   buttonVariant = number !== undefined && curInvalidBlocks.includes(number) && isOpen ? "danger" : buttonVariant;
 
@@ -33,7 +36,7 @@ const HeadlineQuestionContainer = ({ text, number, initOpen = false, children, i
       <Button
         id={`headlineButton-${number}`}
         {...buttonProps}
-        iconRight={icon}
+        iconRight={iconRight}
         variant={buttonVariant}
         fullWidth
         className={styles.headlineButton}
