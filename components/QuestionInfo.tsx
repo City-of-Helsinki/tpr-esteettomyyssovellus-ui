b896defb3d3eaa6755fc6668136389ff3bd687a3
Anthony Brown <ant.brown@cgi.com>
2022-10-05 17:01:27 +0300

    Changed help sections to use accordions

--- components/QuestionInfo.tsx
+++ components/QuestionInfo.tsx
@@ -1,51 +1,27 @@
-import { useI18n } from "next-localization";
-import { useState } from "react";
-import styles from "./QuestionInfo.module.scss";
+import { Button, Card, useAccordion } from "hds-react";
 import { QuestionInfoProps } from "../types/general";
+import styles from "./QuestionInfo.module.scss";
 
 // usage: display info (dropdown) for each question respectively
-const QuestionInfo = ({ openText, openIcon, closeText, closeIcon, textOnBottom = false, children }: QuestionInfoProps): JSX.Element => {
-  const i18n = useI18n();
-
-  const [showContent, setShowContent] = useState(false);
-  const handleToggleContent = () => {
-    setShowContent(!showContent);
-  };
+const QuestionInfo = ({ openText, openIcon, closeIcon, textOnBottom = false, children }: QuestionInfoProps): JSX.Element => {
+  const { isOpen, buttonProps, contentProps } = useAccordion({ initiallyOpen: false });
 
   return (
-    <>
-      {children && !showContent ? (
-        <>
-          <span
-            className={`${styles.infobutton} ${styles.closed}`}
-            onClick={handleToggleContent}
-            onKeyPress={handleToggleContent}
-            role="button"
-            tabIndex={0}
-            title={i18n.t("common.openInfoWhatIsThis")}
-          >
-            {openIcon ?? null}
-            {openText}
-          </span>
-        </>
-      ) : (
-        <>
-          {textOnBottom ? null : <div>{children}</div>}
-          <span
-            className={styles.infobutton}
-            onClick={handleToggleContent}
-            onKeyPress={handleToggleContent}
-            role="button"
-            tabIndex={0}
-            title={i18n.t("common.generalMainInfoIsOpen")}
-          >
-            {closeIcon ?? null}
-            {closeText}
-          </span>
-          {textOnBottom ? <div>{children}</div> : null}
-        </>
+    <div className={styles.infocontainer}>
+      {textOnBottom && (
+        <Button variant="supplementary" size="small" iconLeft={isOpen ? closeIcon ?? null : openIcon ?? null} {...buttonProps}>
+          {openText}
+        </Button>
       )}
-    </>
+      <Card className={styles.contentcontainer} {...contentProps}>
+        {children}
+      </Card>
+      {!textOnBottom && (
+        <Button variant="supplementary" size="small" iconLeft={isOpen ? closeIcon ?? null : openIcon ?? null} {...buttonProps}>
+          {openText}
+        </Button>
+      )}
+    </div>
   );
 };
 
