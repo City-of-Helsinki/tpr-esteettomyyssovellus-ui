7f288b7d6e3f3488909a2bfddf78d82c85408978
pe5 <eero.perola@gmail.com>
2026-09-23 16:21:43 +0300

    Added error messaging

--- components/QuestionFormCtrlButtons.tsx
+++ components/QuestionFormCtrlButtons.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { IconArrowRight, IconArrowLeft } from "hds-react";
+import React, { useState } from "react";
+import { IconArrowRight, IconArrowLeft, Notification } from "hds-react";
 import { useRouter } from "next/router";
 import { useI18n } from "next-localization";
 import SaveSpinner from "./common/SaveSpinner";
@@ -51,6 +51,7 @@ const QuestionFormCtrlButtons = ({
   // const isContinueClicked = useAppSelector((state) => state.formReducer.isContinueClicked);
   const user = useAppSelector((state) => state.generalSlice.user);
   const checksum = useAppSelector((state) => state.generalSlice.checksum);
+  const [saveError, setSaveError] = useState(false);
 
   const handleCancel = (): void => {
     // TODO: Add errorpage
@@ -147,9 +148,13 @@ const QuestionFormCtrlButtons = ({
   };
 
   const handleSaveDraftClick = async () => {
+    setSaveError(false);
     dispatch(setSaving({ draft: true }));
     try {
       await saveData(true);
+    } catch (error) {
+      console.error("Error saving draft", error);
+      setSaveError(true);
     } finally {
       dispatch(setSaving({ draft: false }));
     }
@@ -193,10 +198,15 @@ const QuestionFormCtrlButtons = ({
 
   const handlePreviewClick = async () => {
     if (validateForm()) {
+      setSaveError(false);
       dispatch(setSaving({ preview: true }));
       let entranceId: number;
       try {
         entranceId = await saveData(true);
+      } catch (error) {
+        console.error("Error preparing preview", error);
+        setSaveError(true);
+        return;
       } finally {
         dispatch(setSaving({ preview: false }));
       }
@@ -217,10 +227,15 @@ const QuestionFormCtrlButtons = ({
     setMeetingRoomSaveComplete(false);
 
     if (validateForm()) {
+      setSaveError(false);
       dispatch(setSaving({ meetingRoom: true }));
       let entranceId: number;
       try {
         entranceId = await saveData(false);
+      } catch (error) {
+        console.error("Error saving meeting room", error);
+        setSaveError(true);
+        return;
       } finally {
         dispatch(setSaving({ meetingRoom: false }));
       }
@@ -233,85 +248,92 @@ const QuestionFormCtrlButtons = ({
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        {hasCancelButton && (formId === 0 || formId === 1) ? (
-          <Button variant="secondary" iconLeft={<IconArrowLeft />} onClickHandler={handleCancel} disabled={isSavingDraft || isSavingPreview}>
-            {i18n.t("questionFormControlButtons.quit")}
-          </Button>
-        ) : null}
+    <>
+      {saveError && (
+        <Notification label={i18n.t("common.message.saveFailed.title")} type="error">
+          {i18n.t("common.message.saveFailed.message")}
+        </Notification>
+      )}
+      <div className={styles.container}>
+        <div className={styles.left}>
+          {hasCancelButton && (formId === 0 || formId === 1) ? (
+            <Button variant="secondary" iconLeft={<IconArrowLeft />} onClickHandler={handleCancel} disabled={isSavingDraft || isSavingPreview}>
+              {i18n.t("questionFormControlButtons.quit")}
+            </Button>
+          ) : null}
+        </div>
+        <div className={styles.right}>
+          {hasValidateButton ? (
+            <Button variant="secondary" onClickHandler={handleValidateClick} disabled={isSavingDraft || isSavingPreview}>
+              {i18n.t("questionFormControlButtons.verifyInformation")}
+            </Button>
+          ) : null}
+
+          {hasSaveDraftButton && formId === 0 ? (
+            <Button
+              variant="secondary"
+              onClickHandler={handleSaveDraftClick}
+              disabled={isSavingDraft || isSavingPreview}
+              iconRight={
+                isSavingDraft ? (
+                  <SaveSpinner
+                    savingText={i18n.t("questionFormControlButtons.saving")}
+                    savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
+                  />
+                ) : undefined
+              }
+            >
+              {i18n.t("questionFormControlButtons.saveAsIncomplete")}
+            </Button>
+          ) : null}
+
+          {hasPreviewButton && (formId === 0 || formId === 1) ? (
+            <Button
+              variant="primary"
+              onClickHandler={handlePreviewClick}
+              // disabled={!isPreviewActive || !isContinueClicked}
+              disabled={isSavingDraft || isSavingPreview}
+              iconRight={
+                isSavingPreview ? (
+                  <SaveSpinner
+                    savingText={i18n.t("questionFormControlButtons.saving")}
+                    savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
+                  />
+                ) : (
+                  <IconArrowRight />
+                )
+              }
+            >
+              {i18n.t("questionFormControlButtons.preview")}
+            </Button>
+          ) : null}
+
+          {hasContinueButton ? (
+            <Button variant="primary" iconRight={<IconArrowRight />} onClickHandler={handleContinueClick}>
+              {i18n.t("accessibilityForm.continue")}
+            </Button>
+          ) : null}
+
+          {hasSaveMeetingRoomButton && formId >= 2 ? (
+            <Button
+              variant="primary"
+              onClickHandler={handleSaveMeetingRoomClick}
+              disabled={isSavingDraft || isSavingPreview || isSavingMeetingRoom}
+              iconRight={
+                isSavingMeetingRoom ? (
+                  <SaveSpinner
+                    savingText={i18n.t("questionFormControlButtons.saving")}
+                    savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
+                  />
+                ) : undefined
+              }
+            >
+              {i18n.t("questionFormControlButtons.saveMeetingRoom")}
+            </Button>
+          ) : null}
+        </div>
       </div>
-      <div className={styles.right}>
-        {hasValidateButton ? (
-          <Button variant="secondary" onClickHandler={handleValidateClick} disabled={isSavingDraft || isSavingPreview}>
-            {i18n.t("questionFormControlButtons.verifyInformation")}
-          </Button>
-        ) : null}
-
-        {hasSaveDraftButton && formId === 0 ? (
-          <Button
-            variant="secondary"
-            onClickHandler={handleSaveDraftClick}
-            disabled={isSavingDraft || isSavingPreview}
-            iconRight={
-              isSavingDraft ? (
-                <SaveSpinner
-                  savingText={i18n.t("questionFormControlButtons.saving")}
-                  savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
-                />
-              ) : undefined
-            }
-          >
-            {i18n.t("questionFormControlButtons.saveAsIncomplete")}
-          </Button>
-        ) : null}
-
-        {hasPreviewButton && (formId === 0 || formId === 1) ? (
-          <Button
-            variant="primary"
-            onClickHandler={handlePreviewClick}
-            // disabled={!isPreviewActive || !isContinueClicked}
-            disabled={isSavingDraft || isSavingPreview}
-            iconRight={
-              isSavingPreview ? (
-                <SaveSpinner
-                  savingText={i18n.t("questionFormControlButtons.saving")}
-                  savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
-                />
-              ) : (
-                <IconArrowRight />
-              )
-            }
-          >
-            {i18n.t("questionFormControlButtons.preview")}
-          </Button>
-        ) : null}
-
-        {hasContinueButton ? (
-          <Button variant="primary" iconRight={<IconArrowRight />} onClickHandler={handleContinueClick}>
-            {i18n.t("accessibilityForm.continue")}
-          </Button>
-        ) : null}
-
-        {hasSaveMeetingRoomButton && formId >= 2 ? (
-          <Button
-            variant="primary"
-            onClickHandler={handleSaveMeetingRoomClick}
-            disabled={isSavingDraft || isSavingPreview || isSavingMeetingRoom}
-            iconRight={
-              isSavingMeetingRoom ? (
-                <SaveSpinner
-                  savingText={i18n.t("questionFormControlButtons.saving")}
-                  savingFinishedText={i18n.t("questionFormControlButtons.savingFinished")}
-                />
-              ) : undefined
-            }
-          >
-            {i18n.t("questionFormControlButtons.saveMeetingRoom")}
-          </Button>
-        ) : null}
-      </div>
-    </div>
+    </>
   );
 };
 export default QuestionFormCtrlButtons;
